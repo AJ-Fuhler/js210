8d3f566582e5303d3fecc496bbde175216feb8c1
AJ Fuhler <aart.fuhler@gmail.com>
2025-06-02 10:50:06 -0500

    added assignment 13

---
+++ lesson3/assignment13.js
@@ -0,0 +1,24 @@
+function generatePattern(nStars) {
+  let lastRowString = '';
+
+  for (let lineNumber = 1; lineNumber <= nStars; lineNumber += 1) {
+    lastRowString += String(lineNumber);
+  }
+
+  let width = lastRowString.length;
+
+  for (let lineNumber = 1; lineNumber <= nStars; lineNumber += 1) {
+    let string = '';
+
+    for (let digit = 1; digit <= lineNumber; digit += 1) {
+      string += String(digit);
+    }
+
+    let numberOfStars = width - string.length;
+    string += '*'.repeat(numberOfStars);
+
+    console.log(string);
+  }
+}
+
+generatePattern(20);
