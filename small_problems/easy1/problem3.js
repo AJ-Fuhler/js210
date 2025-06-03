c7c5980c92e6d4127fd2b1226a1b6d79e16c70df
AJ Fuhler <aart.fuhler@gmail.com>
2025-06-03 11:33:56 -0500

    added further exploration to problem3 of easy1

--- small_problems/easy1/problem3.js
+++ small_problems/easy1/problem3.js
@@ -2,13 +2,32 @@ rlSync = require('readline-sync');
 
 const SQMETERS_TO_SQFEET = 10.7639;
 
-console.log('Enter the length of the room in meters:');
+let unit;
+
+
+do {
+  console.log('Please choose between these two units: (sqft/m2)')
+  unit = rlSync.prompt();
+} while (unit !== 'sqft' && unit !== 'm2')
+
+
+
+console.log(`Enter the length of the room in ${unit}:`);
 let length = Number(rlSync.prompt());
 
-console.log('Enter the width of the room in meters:');
+console.log(`Enter the width of the room in ${unit}:`);
 let width = Number(rlSync.prompt());
 
-let squareMeters = (length * width).toFixed(2);
-let sqft = (squareMeters * SQMETERS_TO_SQFEET).toFixed(2);
+let squareMeters;
+let sqft;
 
-console.log(`The area of the room is ${squareMeters} square meters (${sqft} square feet).`)
+if (unit === 'm2') {
+  squareMeters = (length * width);
+  sqft = (squareMeters * SQMETERS_TO_SQFEET);
+} else {
+  sqft = (length * width);
+  squareMeters = (sqft / SQMETERS_TO_SQFEET);
+}
+
+
+console.log(`The area of the room is ${squareMeters.toFixed(2)} square meters (${sqft.toFixed(2)} square feet).`)
