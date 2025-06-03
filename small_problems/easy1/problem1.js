2471ceacdfdeefdd8b37c6c27686a120ee41924a
AJ Fuhler <aart.fuhler@gmail.com>
2025-06-03 11:01:01 -0500

    added further exploration to problem1 of easy1

--- small_problems/easy1/problem1.js
+++ small_problems/easy1/problem1.js
@@ -1,3 +1,17 @@
 for (let num = 1; num <= 99; num += 2) {
   console.log(num);
 }
+
+// further exploration
+
+function logOddNumbers(num1, num2) {
+  if (num1 % 2 !== 1 || num2 % 2 !== 1) {
+    console.log("You have to provide 2 odd numbers.")
+    return;
+  }
+  for (let num = num1; num <= num2; num += 2) {
+    console.log(num);
+  }
+}
+
+logOddNumbers(45, 67);
