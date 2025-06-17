f407cd37087f3ccd88c062813f886644b42ae179
AJ Fuhler <aart.fuhler@gmail.com>
2025-06-17 15:22:44 -0500

    added sum_of_sums solution

---
+++ small_problems/list_processing/sum_of_sums.js
@@ -0,0 +1,10 @@
+const sum = numbers => numbers.reduce((sum, num) => sum + num, 0);
+
+const sumOfSums = numbers => {
+  return sum(numbers.map((_, idx) => sum(numbers.slice(0, idx + 1))));
+};
+
+console.log(sumOfSums([3, 5, 2]));        // (3) + (3 + 5) + (3 + 5 + 2) --> 21
+console.log(sumOfSums([1, 5, 7, 3]));     // (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+console.log(sumOfSums([4]));              // 4
+console.log(sumOfSums([1, 2, 3, 4, 5]));  // 35
